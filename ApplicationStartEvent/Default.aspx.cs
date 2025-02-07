c8b1b20f6108427a8c97ff3f1fe12a9052497173
DesaleRohit <rohitdesale2005@gmail.com>
2025-02-07 18:42:51 +0530

    [ADDED]:Rohit - Application counting event

---
+++ ApplicationStartEvent/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+namespace ApplicationStartEvent
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+            Response.Write("Total Visitor : " + Application["Visitor Count"]);
+
+            Response.Write("<br>");
+
+            Session["Session Count"] = (int)Session["Session Count"] + 1;
+            Response.Write("Total Session : " + Session["Session Count"]);
+        }
+    }
+}
---
+++ ApplicationStartEvent/Global.asax.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Xml.Linq;
+
+namespace ApplicationStartEvent
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            Application["Visitor Count"] = 0;
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            Session["Session Count"] = 0;
+            Application["Visitor Count"] = (int)Application["Visitor Count"] + 1;
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
